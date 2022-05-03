630639c576eaddcc4a28d1e599f5e687e9032568
scottbooma <scottbooma@gmail.com>
2022-05-03 17:52:07 -0400

    Add function that passes tests but has type error

--- resistor-color.ts
+++ resistor-color.ts
@@ -1,3 +1,42 @@
-export const colorCode = () => {};
+export const COLORS: string[] = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
 
-export const COLORS = undefined;
+type Colors = {
+  black: number;
+  brown: number;
+  red: number;
+  orange: number;
+  yellow: number;
+  green: number;
+  blue: number;
+  grey: number;
+  violet: number;
+  white: number;
+};
+
+const colors: Colors = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const colorCode = (color: string): number => {
+  return colors[color];
+};
